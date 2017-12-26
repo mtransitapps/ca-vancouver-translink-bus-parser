0f0b55ddfe29ccb4fcddfd50d484f8fb47bbe8bb
Mathieu Méa <mathieu.mea@gmail.com>
2017-12-26 11:21:06 -0500

    Compatibility with latest update.

--- src/org/mtransit/parser/ca_vancouver_translink_bus/VancouverTransLinkBusAgencyTools.java
+++ src/org/mtransit/parser/ca_vancouver_translink_bus/VancouverTransLinkBusAgencyTools.java
@@ -543,7 +543,7 @@ public class VancouverTransLinkBusAgencyTools extends DefaultAgencyTools {
 	@Override
 	public int compareEarly(long routeId, List<MTripStop> list1, List<MTripStop> list2, MTripStop ts1, MTripStop ts2, GStop ts1GStop, GStop ts2GStop) {
 		if (ALL_ROUTE_TRIPS2.containsKey(routeId)) {
-			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
+			return ALL_ROUTE_TRIPS2.get(routeId).compare(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop, this);
 		}
 		return super.compareEarly(routeId, list1, list2, ts1, ts2, ts1GStop, ts2GStop);
 	}
@@ -559,7 +559,7 @@ public class VancouverTransLinkBusAgencyTools extends DefaultAgencyTools {
 	@Override
 	public Pair<Long[], Integer[]> splitTripStop(MRoute mRoute, GTrip gTrip, GTripStop gTripStop, ArrayList<MTrip> splitTrips, GSpec routeGTFS) {
 		if (ALL_ROUTE_TRIPS2.containsKey(mRoute.getId())) {
-			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()));
+			return SplitUtils.splitTripStop(mRoute, gTrip, gTripStop, routeGTFS, ALL_ROUTE_TRIPS2.get(mRoute.getId()), this);
 		}
 		return super.splitTripStop(mRoute, gTrip, gTripStop, splitTrips, routeGTFS);
 	}
@@ -1369,15 +1369,17 @@ public class VancouverTransLinkBusAgencyTools extends DefaultAgencyTools {
 			}
 		} else if (mTrip.getRouteId() == 410l) {
 			if (Arrays.asList( //
-					BRIGHOUSE_STATION, //
+					StringUtils.EMPTY, // <>
+					BRIGHOUSE_STATION, // <>
 					BOUNDARY_RD, //
 					_22ND_ST_STATION //
 					).containsAll(headsignsValues)) {
 				mTrip.setHeadsignString(_22ND_ST_STATION, mTrip.getHeadsignId());
 				return true;
 			} else if (Arrays.asList( //
+					StringUtils.EMPTY, // <>
+					BRIGHOUSE_STATION, // <>
 					BRIGHOUSE, //
-					BRIGHOUSE_STATION, //
 					HAMILTON_PARK, //
 					QUEENSBORO, //
 					RAILWAY //
