871e227e66e64cf99c70725f262ee8555532fa19
Mathieu Méa <mathieu.mea@gmail.com>
2018-07-26 15:11:03 -0400

    Compatible with Current/Next GTFS data

--- src/org/mtransit/parser/ca_vancouver_translink_bus/VancouverTransLinkBusAgencyTools.java
+++ src/org/mtransit/parser/ca_vancouver_translink_bus/VancouverTransLinkBusAgencyTools.java
@@ -58,6 +58,11 @@ public class VancouverTransLinkBusAgencyTools extends DefaultAgencyTools {
 	}
 
 	@Override
+	public boolean excludingAll() {
+		return this.serviceIds != null && this.serviceIds.isEmpty();
+	}
+
+	@Override
 	public boolean excludeCalendar(GCalendar gCalendar) {
 		if (this.serviceIds != null) {
 			return excludeUselessCalendar(gCalendar, this.serviceIds);
